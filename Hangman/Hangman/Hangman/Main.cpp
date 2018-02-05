3a9fbc6620a6f72e313923df849ce679b9c8ace0
sami10015 <sami.ellougani1@marist.edu>
2018-02-05 15:05:09 -0500

    Borderline finished with title screen

--- Hangman/Hangman/Hangman/Main.cpp
+++ Hangman/Hangman/Hangman/Main.cpp
@@ -4,6 +4,8 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Hello SFML", sf::Style::Fullscreen);
+	window.setFramerateLimit(60);
+	window.setVerticalSyncEnabled(false);
 
 	//Create header file for title screen
 	TitleScreen titleScreen(window);
--- Hangman/Hangman/Hangman/TitleScreen.cpp
+++ Hangman/Hangman/Hangman/TitleScreen.cpp
@@ -3,11 +3,30 @@
 TitleScreen::TitleScreen(sf::RenderWindow & window){
 	font.loadFromFile("Fonts/Pacifico.ttf");
 	title.setFont(font);
-	title.setOrigin(title.getLocalBounds().width / 2, title.getLocalBounds().height / 2);
-	title.setPosition(window.getSize().x * .4 , window.getSize().y * .4);
 	title.setCharacterSize(60);
 	title.setString("Hangman");
+	title.setOrigin(title.getLocalBounds().width / 2, title.getLocalBounds().height / 2);
+	title.setPosition(window.getSize().x * .5, window.getSize().y * .4);
 
+	playText.setFont(font);
+	playText.setCharacterSize(50);
+	playText.setString("Play");
+	playText.setOrigin(playText.getLocalBounds().width / 2, playText.getLocalBounds().height / 2);
+	playText.setPosition(window.getSize().x * .5, window.getSize().y * .75);
+
+	int playAlpha = 255;
+	playText.setFillColor(sf::Color(255, 255, 255, playAlpha));
+
+	exitText.setFont(font);
+	exitText.setCharacterSize(50);
+	exitText.setString("Exit");
+	exitText.setOrigin(playText.getLocalBounds().width / 2, playText.getLocalBounds().height / 2);
+	exitText.setPosition(window.getSize().x * .5, window.getSize().y * .85);
+	
+	int exitAlpha = 255;
+	exitText.setFillColor(sf::Color(255, 255, 255, exitAlpha));
+
+	int playerSelection = 0;
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event))
@@ -20,20 +39,49 @@ TitleScreen::TitleScreen(sf::RenderWindow & window){
 				//Key pressed
 				case sf::Event::KeyPressed:
 					if (event.key.code == sf::Keyboard::Down) {
-						//TODO: Indicate which option is selected
-					}
-					else if (event.key.code == sf::Keyboard::Up) {
-						//TODO: Indicate which option is selected
-					}
-					else if (event.key.code == sf::Keyboard::Escape) {
+						if (playerSelection == 0) {
+							playerSelection = 1;
+							playAlpha = 255;
+							playText.setFillColor(sf::Color(255, 255, 255, playAlpha));
+						}
+					} else if (event.key.code == sf::Keyboard::Up) {
+						if (playerSelection == 1) {
+							playerSelection = 0;
+							exitAlpha = 255;
+							exitText.setFillColor(sf::Color(255, 255, 255, exitAlpha));
+						}
+					} else if (event.key.code == sf::Keyboard::Return) {
+						if (playerSelection == 0) {
+							//TODO: Start Game
+						} else {
+							window.close();
+						}
+					} else if (event.key.code == sf::Keyboard::Escape) {
 						window.close();
 					}
 				default:
 					break;
 			}
 		}
+
+		//Fade in and out play text
+		if (playerSelection == 0) {
+			if (playAlpha < 0) {
+				playAlpha = 255;
+			}
+			playText.setFillColor(sf::Color(255, 255, 255, playAlpha));
+			playAlpha -= 8;
+		} else if(playerSelection == 1){ //Fade in and out exit text
+			if (exitAlpha < 0) {
+				exitAlpha = 255;
+			}
+			exitText.setFillColor(sf::Color(255, 255, 255, exitAlpha));
+			exitAlpha -= 8;
+		}
 		window.clear();
 		window.draw(title);
+		window.draw(playText);
+		window.draw(exitText);
 		window.display();
 	}
 }
